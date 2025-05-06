b88f595e50853e4e048293d7746ec683d2f6a0c8
Frouzin <matheusereis44@gmail.com>
2025-05-06 13:43:03 -0300

    alterações em aula

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        Pessoa pessoa = new Pessoa("Marco", 21);
+        Pessoa pessoa = new Pessoa("Marco", 22);
         Pessoa pessoa2 = new Pessoa("Antonio", 22);
 
         if(pessoa.idade>pessoa2.idade){
             System.out.println(pessoa.nome + " é mais velho que " + pessoa2.nome);
         }
-        else {
+        else if (pessoa.idade == pessoa2.idade) {
+            System.out.println(pessoa2.nome + " tem a mesma idade que " + pessoa.nome);
+        }
+        else{
             System.out.println(pessoa2.nome + " é mais velho que " + pessoa.nome);
         }
 
@@ -15,6 +18,13 @@ public class Main {
         }
         else{
             System.out.println(pessoa.nome + " não é de maior");
+        }
+
+        if (pessoa2.isMaiorDeIdade()){
+            System.out.println(pessoa2.nome + " é de maior");
+        }
+        else{
+            System.out.println(pessoa2.nome + " não é de maior");
         }
     }
 }
